f744c2fc217284d6ae87751f10aea84083920f69
Алексей <godyl1997@gmail.com>
2018-10-28 20:46:56 +0300

    Add Sort in Files

--- CounterChangeFile/Controllers/FilesController.cs
+++ CounterChangeFile/Controllers/FilesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CounterChangeFile.Models;
+using CounterChangeFile.Models.Enums;
 
 namespace CounterChangeFile.Controllers
 {
@@ -18,10 +19,22 @@ namespace CounterChangeFile.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index(int Id)
+        public async Task<IActionResult> Index(int Id, SortFiles sortFiles = SortFiles.NumberOfChangeDefault)
         {
             IQueryable<Files> files = _context.Files.Where(x => x.Analysis.Id == Id);
-            return View(files);
+            ViewData["sortFiles"] = sortFiles == SortFiles.NumberOfChangeAsc ? SortFiles.NumberOfChangeDesc : SortFiles.NumberOfChangeAsc;
+            switch(sortFiles)
+            {
+                case SortFiles.NumberOfChangeAsc:
+                    files = files.OrderBy(s => s.NumberOfChange);
+                    break;
+                case SortFiles.NumberOfChangeDesc:
+                    files = files.OrderByDescending(s => s.NumberOfChange);
+                    break;
+                
+            }
+           
+            return View(await files.AsNoTracking().ToListAsync());
         }
 
  
---
+++ CounterChangeFile/Models/Enums/SortFiles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CounterChangeFile.Models.Enums
+{
+    public enum SortFiles
+    {
+        NumberOfChangeAsc,
+        NumberOfChangeDesc,
+        NumberOfChangeDefault
+    }
+}
